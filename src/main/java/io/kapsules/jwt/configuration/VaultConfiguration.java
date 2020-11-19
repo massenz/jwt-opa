8bba7a017058bfbb8494a95f9c3237530241e0e9
Seung Kim <seunkim@adobe.com>
2020-11-19 12:22:40 -0800

    Fixed build and test Mock private/public key objects so we don't need to have the `pem` files in test resources

--- src/main/java/io/kapsules/jwt/configuration/VaultConfiguration.java
+++ src/main/java/io/kapsules/jwt/configuration/VaultConfiguration.java
@@ -1,20 +1,18 @@
 package io.kapsules.jwt.configuration;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import io.kapsules.jwt.thirdparty.PemUtils;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.interfaces.ECKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import io.kapsules.jwt.thirdparty.PemUtils;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
@@ -52,8 +50,12 @@ public class VaultConfiguration {
 
     // See this: https://github.com/auth0/java-jwt/issues/270
     // Keys generated with:
+    // 1. generate the EC param
     //    openssl ecparam -name prime256v1 -genkey -noout -out ec-key.pem
+    // 2. generate EC private key
     //    openssl pkcs8 -topk8 -inform pem -in ec-key.pem -outform pem -nocrypt -out ec-key-1.pem
+    // 3. generate EC public key
+    // openssl ec -in ec-key-1.pem -pubout -out public.pem
     PrivateKey pk = PemUtils.readPrivateKeyFromFile(privateKey, ELLIPTIC_CURVE);
     if (pk == null) {
       log.error("Could not read Public key");
