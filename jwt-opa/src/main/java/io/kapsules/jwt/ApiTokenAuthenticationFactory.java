9640646d3808167001302d6dddbd9b019c10d391
Marco Massenzio <marco@alertavert.com>
2020-12-17 14:11:58 -0800

    Fixed gradle deprecation & test coverage configuration

--- jwt-opa/src/main/java/io/kapsules/jwt/ApiTokenAuthenticationFactory.java
+++ jwt-opa/src/main/java/io/kapsules/jwt/ApiTokenAuthenticationFactory.java
@@ -3,14 +3,12 @@ package io.kapsules.jwt;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static io.kapsules.jwt.JwtTokenProvider.ROLE;
 
@@ -43,7 +41,7 @@ public class ApiTokenAuthenticationFactory {
       // We need to use here the superclass #setAuthenticated() because the method
       // in the ApiTokenAuth class has been disabled and throws if called.
       //
-      auth = new ApiTokenAuthentication(token, "", Collections.EMPTY_LIST, null);
+      auth = new ApiTokenAuthentication(token, "", Collections.emptyList(), null);
     }
     return auth;
   }
