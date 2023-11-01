df0e910b5658c643f232899abb651c174e8e4c87
Marco Massenzio <m@massenz.io>
2023-10-31 23:34:51 -0700

    Cleaned up build.gradle structure
    Also updated the run-example script
    Updated OPA version to latest

--- run-example.sh
+++ run-example.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2021 AlertAvert.com.  All rights reserved.
+# Copyright (c) 2021-2023 AlertAvert.com.  All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -13,6 +13,14 @@ OPA_SERVER=http://localhost:${OPA_PORT}
 POLICY_API=${OPA_SERVER}/v1/policies/userauth
 
 docker compose --project-name jwt-opa up -d
+
+# Sometimes the OPA server takes a while to start, so we wait for it to be ready
+READY=""
+while [[ -z ${READY} ]]; do
+    echo "Waiting for OPA server to start..."
+    sleep 1
+    READY=$(docker logs opa 2>&1 | grep "Initializing server.")
+done
 if [[ $(curl -s ${POLICY_API} | jq .result.id) != "userauth" ]]; then
     echo "Uploading userauth Policy"
     curl -T "${WORKDIR}/webapp-example/src/main/rego/jwt_auth.rego" -X PUT ${POLICY_API}
